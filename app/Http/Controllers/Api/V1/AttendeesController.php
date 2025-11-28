d1efd75943c66db1788c865d0556fefecabc41fa
bilalrao-web <bilal.rao@tekcroft.com>
2025-11-28 18:04:03 +0500

    function def in controllers

--- app/Http/Controllers/Api/V1/AttendeesController.php
+++ app/Http/Controllers/Api/V1/AttendeesController.php
@@ -6,5 +6,43 @@ use Illuminate\Http\Request;
 
 class AttendeesController extends ApiController
 {
-    //
+     /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show()
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update()
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy()
+    {
+        //
+    }
 }
